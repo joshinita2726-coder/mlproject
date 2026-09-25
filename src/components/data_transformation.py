ee61373b5ac131840b78c915f79ae6572df81a5c
Nita <joshinita2726@gmail.com>
2026-09-25 19:05:59 +0530

    Hyperparameter training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -229,7 +229,6 @@ class DataTransformation:
                 self.data_transformation_config
                 .preprocessor_obj_file_path
             )
-
         except Exception as e:
 
             raise CustomException(e, sys)
